375885f5591bd0391eabd9269f10b1546957d755
fermayo <fermayo@gmail.com>
2015-02-09 23:04:48 +0100

    moved packer to tutum/node

--- packer/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-mkdir -p staging
-rm -f staging/*.box
-wget https://vagrantcloud.com/ubuntu/boxes/trusty64/versions/14.04/providers/virtualbox.box -P staging
-tar zxvf staging/*.box -C staging/
-rm -f staging/*.box
