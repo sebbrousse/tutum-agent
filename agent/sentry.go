b96aee1ca70a69bb5c381fbc066848dd445a71c7
fermayo <fermayo@gmail.com>
2015-04-10 11:43:15 +0200

    remove DSN from public repo

--- agent/sentry.go
+++ agent/sentry.go
@@ -5,10 +5,10 @@ import (
 )
 
 var sentryClient *raven.Client = nil
-var DSN string = "https://af8f9402046b434781f9e09ab6da4b92:81139d3c30b64df8ae1601374e2ed698@app.getsentry.com/40515"
+var DSN string
 
 func getSentryClient() *raven.Client {
-	if sentryClient == nil {
+	if sentryClient == nil && DSN != "" {
 		client, _ := raven.NewClient(DSN, nil)
 		sentryClient = client
 	}
