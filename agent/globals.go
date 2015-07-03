03c3f846e573443851609fa2c3abb3d37cfdb2a0
fermayo <fermayo@gmail.com>
2015-07-03 09:44:09 +0200

    back to DEV version

--- agent/globals.go
+++ agent/globals.go
@@ -29,7 +29,7 @@ var (
 )
 
 const (
-	VERSION               = "0.16.0"
+	VERSION               = "0.16.0-dev"
 	defaultCertCommonName = ""
 	defaultDockerHost     = "tcp://0.0.0.0:2375"
 	defaultTutumHost      = "https://dashboard.tutum.co/"
