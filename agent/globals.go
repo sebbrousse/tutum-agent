43505214339c27652182022259d9f6a6c137a16a
fermayo <fermayo@gmail.com>
2015-10-09 10:55:53 +0200

    bump version

--- agent/globals.go
+++ agent/globals.go
@@ -30,7 +30,7 @@ var (
 )
 
 const (
-	VERSION               = "0.18.2-dev"
+	VERSION               = "0.19.0"
 	defaultCertCommonName = ""
 	defaultDockerHost     = "tcp://0.0.0.0:2375"
 	defaultTutumHost      = "https://dashboard.tutum.co/"
